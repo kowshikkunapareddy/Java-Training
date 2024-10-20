1ed465edfbe7d376e4d4fd369fcdad5a7667a233
kowshik kunapareddy <kowshik.kunapareddy@gmail.com>
2024-10-20 09:53:53 +0530

    minor changes to basic project

--- src/day1/First_Step.java
+++ src/day1/First_Step.java
@@ -4,10 +4,11 @@ public class First_Step {
     public class Main {
     }
     public static void main(String[] args) {
-        System.out.printf("Java!");
-        System.out.printf(10*8);
+        System.out.println("Java!");
+        System.out.println(10*8);
 
     }
 }
+
 
 
