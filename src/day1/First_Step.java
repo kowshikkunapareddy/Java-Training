ac8ff4400d31bbec29326879a4c36b047a8cfce9
kowshik kunapareddy <kowshik.kunapareddy@gmail.com>
2024-10-20 09:44:47 +0530

    added basic project

---
+++ src/day1/First_Step.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class First_Step {
+    public class Main {
+    }
+    public static void main(String[] args) {
+        System.out.printf("Java!");
+        System.out.printf(10*8);
+
+    }
+}
+
+
