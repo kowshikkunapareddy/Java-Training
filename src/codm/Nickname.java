65c28780f8436b89e2ac16829a03e012b8b3de87
kowshik kunapareddy <kowshik.kunapareddy@gmail.com>
2024-11-04 23:07:33 +0530

    Barat

---
+++ src/codm/Nickname.java
@@ -0,0 +1,4 @@
+package codm;
+
+public class Nickname {
+}
---
+++ src/day3/Operations.java
@@ -0,0 +1,22 @@
+package day3;
+
+    public class Variables {
+        public static void main(String[] args) {
+
+            // 1. Arithmetic Operations
+
+            int a=10, b=20;
+            System.out.println("Sum of a and b is:"+(a+b));
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
