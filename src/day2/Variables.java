abe09092383cfe76249e0efb2ac67525c67b7cf7
kowshik kunapareddy <kowshik.kunapareddy@gmail.com>
2024-11-05 20:53:15 +0530

    swapping

--- src/day2/Variables.java
+++ src/day2/Variables.java
@@ -1,4 +1,21 @@
 package day2;
 
 public class Variables {
+    public static void main(String[] args) {
+
+    /*int a= 10, b= 20;
+
+        System.out.println(a+b);*/
+
+      /*int a=1;
+        int b=20;
+        int c=30;
+        System.out.println(b-a+c);*/
+
+  int a=100, b=500, c=1000;
+  System.out.println("value of a and b is:" +(a+b));
+
+  char k = 'c';
+  System.out.println(k);
+    }
 }
--- src/day3/Operations.java
+++ src/day3/Operations.java
@@ -1,22 +1,17 @@
 package day3;
 
-    public class Variables {
-        public static void main(String[] args) {
+    public class Operations {
+        public static void main(String[] args)
+
+        {
 
             // 1. Arithmetic Operations
 
-            int a=10, b=20;
+            int a=50, b=20;
             System.out.println("Sum of a and b is:"+(a+b));
-
+            System.out.println("Sum of a and b is:"+(a-b));
+            System.out.println("Sum of a and b is:"+(a*b));
+            System.out.println("Sum of a and b is:"+(a/b));
+            System.out.println("Sum of a and b is:"+(a%b));
         }
-
-
-
-
-
-
-
-
-
-
     }
