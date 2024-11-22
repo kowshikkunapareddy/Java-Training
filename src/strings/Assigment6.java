9143700a16d9f8776e769fe47ecaef67cb19e913
kowshik kunapareddy <kowshik.kunapareddy@gmail.com>
2024-11-22 18:23:39 +0530

    assigments

--- src/strings/Assigment6.java
+++ src/strings/Assigment6.java
@@ -1,4 +1,21 @@
 package strings;
 
 public class Assigment6 {
+
+    public static void main(String[] args) {
+
+        String Name = "Barat";
+        String Char = "is good";
+        String Chars = new String("is bad");
+
+
+
+        StringBuilder combined = new StringBuilder();
+
+        combined.append(Name).append("  ").append(Char);
+
+        System.out.println(combined);
+        System.out.println(Name.concat("  ").concat(Chars));
+
+}
 }
--- src/strings/Assigmnet5.java
+++ src/strings/Assigmnet5.java
@@ -1,4 +1,29 @@
 package strings;
 
 public class Assigmnet5 {
-}
+    public static void main(String[] args) {
+
+
+        String MI8 = "our lives are not defined by any one action, our lives sum of our choices";
+        String reverse ="";
+
+        for (int i = MI8.length() - 1; i >= 0; i--) {
+
+            reverse= reverse + MI8.charAt(i);
+        }
+
+        System.out.println(reverse);
+
+        if (reverse == new String(MI8)) {
+
+            System.out.println("Given string is Palendrome");
+
+        } else {
+                {
+                    System.out.println("Given string is not palendrome");
+
+                }
+            }
+
+        }
+    }
