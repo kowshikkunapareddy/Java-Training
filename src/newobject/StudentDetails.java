28ec2e4e65cf85faa98f90511ec957fe50724dcd
kowshik kunapareddy <kowshik.kunapareddy@gmail.com>
2024-11-23 16:01:24 +0530

    Object

---
+++ src/newobject/StudentDetails.java
@@ -0,0 +1,19 @@
+package newobject;
+
+public class StudentDetails {
+
+    String Name = "Kowshik";
+    int Roll_Number = 19;
+    String Grade = "A+";
+
+    String Name1 = "Barat";
+    int Roll_Number1 = 1;
+    String Grade1 = "A+";
+
+    String Name2 = "KK";
+    int Roll_Number2 = 2;
+    String Grade2 = "A";
+
+
+}
+
